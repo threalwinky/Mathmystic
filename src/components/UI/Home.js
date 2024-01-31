65272227497cce39e08f0152c15b903be0c09ca2
threalwinky <voquangvu09112006@gmail.com>
2024-01-31 07:39:57 +0700

    upload files

--- src/components/UI/Home.js
+++ src/components/UI/Home.js
@@ -1,8 +1,73 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import '../css/home.css'
+import girl from '../img/girl.webp';
 export default function Home() {
     return (
-      <h1>
-        MATHMYSTIC XIN CHÀO
-CHẮP CÁNH TƯ DUY, KẾT NỐI TRI THỨC, ÁNH SÁNG CHO MỌI TRÁI TIM
-      </h1>
+      <div className="container">
+        <div className="first">
+          <div>
+          <h1>
+          ​MATHMYSTIC XIN CHÀO
+          </h1>
+          <a>
+CHẮP CÁNH TƯ DUY, KẾT NỐI TRI THỨC, <br></br>
+ÁNH SÁNG CHO MỌI TRÁI TIM
+          </a>
+          </div>
+         
+
+<img src={girl} />
+        </div>
+
+
+
+        <div className="second">
+          
+          <h1>
+          Sản Phẩm Mới Ở Đây
+
+          </h1>
+
+          <a>
+          Limited time price drop!
+          </a>
+
+        </div>
+
+
+        <div className="third">
+         <h1>
+          Bộ sưu tập
+         </h1>
+        </div>
+
+        <div className="fourth">
+         <h1>
+         ​GIỚI THIỆU 
+Chúng tôi sẽ giới thiệu một sản phẩm toán học độc đáo cho tất cả các bạn học sinh và giáo viên. Hơn cả một sản phẩm, đây còn là một cách thức mới, sáng tạo và thú vị để tìm hiểu và trải nghiệm hình học
+
+         </h1>
+        </div>
+
+        <div className="fifth">
+         <h1>
+         ​GIỚI THIỆU 
+Chúng tôi sẽ giới thiệu một sản phẩm toán học độc đáo cho tất cả các bạn học sinh và giáo viên. Hơn cả một sản phẩm, đây còn là một cách thức mới, sáng tạo và thú vị để tìm hiểu và trải nghiệm hình học
+
+         </h1>
+        </div>
+        <div className="sixth">
+         <h1>
+         ​GIỚI THIỆU 
+Chúng tôi sẽ giới thiệu một sản phẩm toán học độc đáo cho tất cả các bạn học sinh và giáo viên. Hơn cả một sản phẩm, đây còn là một cách thức mới, sáng tạo và thú vị để tìm hiểu và trải nghiệm hình học
+
+         </h1>
+        </div>
+        
+      </div>
+
+      
     )
   }
--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -21,7 +21,7 @@ export default function Navbar() {
 function CustomLink({ to, children, ...props }) {
   const resolvedPath = useResolvedPath(to)
   const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-
+  
   return (
     <li className={isActive ? "active" : ""}>
       <Link to={to} {...props}>
