73001b8d820eacbd2d151b23de3c45dfd17d0039
threalwinky <voquangvu09112006@gmail.com>
2024-02-17 03:40:29 +0700

    upload files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
-import { Skills2 } from "./components/Skills2";
-import { Project2 } from "./components/Project2";
-import { Projects } from "./components/Projects";
-import { Project3 } from "./components/Project3";
-import { Project4 } from "./components/Project4";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
-import { ThemedExample } from "./components/Chatbot";
-import { CB } from "./components/ChatbotButton";
-import {Routes, Route, useNavigate} from 'react-router-dom';
-import { translate, Trans, withTranslation } from 'react-i18next';
-import React, { Component } from "react";
+import {NavBar} from './components'
 function App() {
-  
   return (
     <div className="App">
-      <NavBar />
-
-
-      <Banner />
-      <Project2 />
-      <Skills />
-      <Projects />
-      <Project3 />
-      <Skills2 />
-      <Project4 />
-      <Contact />
-      
-      <Footer />
-      <ThemedExample />
-      
+      <NavBar></NavBar>
     </div>
   );
 }
 
-export default withTranslation("translations")(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
-import App from './App';
-import "./App.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-// append app to dom
-ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
-    <BrowserRouter>
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <App />
-    </BrowserRouter>
-    </I18nextProvider>,
-  document.getElementById('root')
 );
+
