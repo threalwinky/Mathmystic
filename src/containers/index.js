2753a8a250e2bee91ce8ccb197de0566567aa203
threalwinky <voquangvu09112006@gmail.com>
2024-02-18 18:31:56 +0700

    upload files

--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as PopUp } from './popup/PopUp'
-export { default as Loading } from './loading/Loading'
-export { default as NotFound } from './notfound/NotFound'
-export { default as Chatbot } from './chatbot/Chatbot'
